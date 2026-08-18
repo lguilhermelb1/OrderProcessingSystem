eff068e1a3be4f6b4edb069f1cdd8ddd3f8c3d00
lguilhermelb1 <luiseuguilherme@gmail.com>
2026-08-18 11:26:44 -0300

    Removed node_modules, dist and build. Created database connection

--- api/dist/modules/products/products.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { CreateProductDto } from './dto/create-product.dto';
-export declare class ProductsService {
-    create(createProductDto: CreateProductDto): string;
-    findAll(): string;
-    findOne(id: number): string;
-    remove(id: number): string;
-}
