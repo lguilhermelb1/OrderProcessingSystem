acd8300f05f5b46355c02ef8c9e242219621eacd
lguilhermelb1 <luiseuguilherme@gmail.com>
2026-08-18 17:20:11 -0300

    App Module and main configurations done. Add RedisService and DatabaseService imports to Products Module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,37 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './modules/products/products.module';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
+import { RedisModule } from './common/redis/redis.module';
+import { DatabaseModule } from './common/database/database.module';
+import Joi from 'joi';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('development', 'production', 'test')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+
+        // PostgreSQL configuration
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().default(5432),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        POSTGRES_MAX_CONNECTIONS: Joi.number().default(20),
+
+        // Redis configuration
+        REDIS_HOST: Joi.string().required(),
+        REDIS_PORT: Joi.number().default(6379),
+        REDIS_PASSWORD: Joi.string().allow('').optional(),
+      }),
+    }),
+    ProductsModule,
+    RedisModule,
+    DatabaseModule
+  ],
 })
 export class AppModule {}
--- api/src/modules/products/products.module.ts
+++ api/src/modules/products/products.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { RedisService } from '../../common/redis/redis.service';
+import { DatabaseService } from '../../common/database/database.service';
 
 @Module({
   controllers: [ProductsController],
-  providers: [ProductsService],
+  providers: [ProductsService, RedisService, DatabaseService],
 })
 export class ProductsModule {}
