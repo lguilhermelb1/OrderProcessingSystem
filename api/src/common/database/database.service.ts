77d6dc1f8436d2cf8fb2b30889830ec78df05030
lguilhermelb1 <luiseuguilherme@gmail.com>
2026-08-18 11:27:22 -0300

    Database connection

--- api/src/common/database/database.service.ts
+++ api/src/common/database/database.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Pool, QueryResult, QueryResultRow } from 'pg';
 
 @Injectable()
-export class DatabaseService {}
+export class DatabaseService implements OnModuleInit, OnModuleDestroy {
+    private readonly logger = new Logger(DatabaseService.name);
+    private pool!: Pool;
+
+    constructor(private readonly configService: ConfigService) {}
+
+    onModuleInit() {
+        this.pool = new Pool({
+            host: this.configService.getOrThrow<string>('POSTGRES_HOST'),
+            port: this.configService.getOrThrow<number>('POSTGRES_PORT'),
+            user: this.configService.getOrThrow<string>('POSTGRES_USER'),
+            password: this.configService.getOrThrow<string>('POSTGRES_PASSWORD'),
+            database: this.configService.getOrThrow<string>('POSTGRES_DB'),
+            max: this.configService.getOrThrow<number>('POSTGRES_MAX_CONNECTIONS'),
+            idleTimeoutMillis: 30000,
+        });
+        
+        this.logger.log('Database connection pool initialized');
+    }
+
+    async query<T extends QueryResultRow = any>(text: string, params?: any[]): Promise<QueryResult<T>> {
+        return this.pool.query<T>(text, params);
+    }
+
+    async onModuleDestroy() {
+        await this.pool.end();
+        this.logger.log('Database connection pool closed');
+    }
+}
